fd51d92de201b1405642d4cfc79ff25c22930bb5
Weriton T. Machado <tom.wtm@gmail.com>
2020-07-26 17:40:18 -0300

    🐛 correção de erro de digitação;

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: `https://tom-machado-desafio-fina.herokuapp.com`,
+  baseURL: `https://tom-machado-desafio-final.herokuapp.com`,
   //baseURL: 'http://localhost:3000',
   mode: 'no-cors',
   headers: {
