c87d845f8a46b85747430c1621fccfa590d0d6e2
Weriton T. Machado <tom.wtm@gmail.com>
2020-07-26 15:49:24 -0300

    🐛 correção na url de conção com API

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://tom-machado-desafio-fina.herokuapp.com/api',
+  baseURL: 'https://tom-machado-desafio-fina.herokuapp.com/',
   //baseURL: 'http://localhost:3000',
   headers: {
     'Content-type': 'application/json',
