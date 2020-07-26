867d1d3c05688620d2e758ac8caa9513abb22a09
Weriton T. Machado <tom.wtm@gmail.com>
2020-07-26 15:58:32 -0300

    🔧configurando CORS;

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(
-  cors({
-    origin: 'https://tom-machado-desafio-final.herokuapp.com',
-    //origin: 'http://localhost:3000',
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 /**
